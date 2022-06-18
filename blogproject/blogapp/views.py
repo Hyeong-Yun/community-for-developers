7703cd44b844bcefed0bb041b193b39124adcadf
HyeongYun <guddbs5545@naver.com>
2022-06-18 15:07:56 +0900

    1th: 대나무숲 만들기 전, 블로그 연습, 글 생성하기 생성

--- blogproject/blogapp/views.py
+++ blogproject/blogapp/views.py
@@ -1,5 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Blog
+from django.utils import timezone
+from .forms import BlogForm
 
 # Create your views here.
 def home(request):
   return render(request, 'index.html')
+
+# 블로그 작성 HTML을 보여 주는 함수
+def new(request):
+  return render(request, 'new.html')
+
+# 블로그 글을 저장해주는 함수
+def create(request):
+  if(request.method == "POST"):
+    post = Blog()
+    post.title = request.POST['title']
+    post.body = request.POST['body']
+    post.date = timezone.now()
+    post.save()
+
+  return redirect('home')
+# django form을 이용해서 입력값을 받는 함수
+# GET 요청 (=입력값을 받을 수 있는 Html을 갖다 줘야함)
+# POST 요청 (= 입력한 내용을 데이터베이스에 저장)
+# 둘 다 처리가 가능한  함수
+
+def formcreate(request):
+    if request.method = 'POST':
+      #입력 내용을 DB에 저장 
+    else:
+      # 입력을 받을 수 있는 HTM을 갖다 주기
+      form = BlogForm()
+    return render(request,'form_create.html', {'form': form})
